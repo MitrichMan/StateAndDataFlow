7de572370038cbc2cfe302911da642ce6ec5d785
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-05-01 19:37:57 +0800

    Made UserManager class

--- StateAndDataFlow/Services/UserManager.swift
+++ StateAndDataFlow/Services/UserManager.swift
@@ -5,4 +5,18 @@
 //  Created by Dmitrii Melnikov on 01.05.2023.
 //
 
-import Foundation
+import Combine
+
+final class UserManager: ObservableObject {
+    @Published var user = User()
+    
+    var nameIsValid: Bool {
+        user.name.count >= 3
+    }
+    
+    init() {}
+    
+    init(user: User) {
+        self.user = user
+    }
+}
