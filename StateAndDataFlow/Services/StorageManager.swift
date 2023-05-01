42decac612434f70dbeee1062200ccf546e39d52
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-05-01 19:45:41 +0800

    made StorageManager

--- StateAndDataFlow/Services/StorageManager.swift
+++ StateAndDataFlow/Services/StorageManager.swift
@@ -5,4 +5,29 @@
 //  Created by Dmitrii Melnikov on 01.05.2023.
 //
 
-import Foundation
+import SwiftUI
+
+final class StorageManager {
+    static let shared = StorageManager()
+    
+    @AppStorage("user") private var userData: Data?
+    
+    private init() {}
+    
+    func save(user: User) {
+        userData = try? JSONEncoder().encode(user)
+    }
+    
+    func fetchUser() -> User {
+        guard let userData else { return User() }
+        let user = try? JSONDecoder().decode(User.self, from: userData)
+        guard let user else { return User() }
+        return user
+    }
+    
+    func clear(userManager: UserManager) {
+        userManager.user.isLoggedIn = false
+        userManager.user.name = ""
+        userData = nil
+    }
+}
